1cb9d56876814c05dd776dbdf66d26f7f9cd8ea0
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-13 17:06:08 -0300

    Arrays

---
+++ js/array.js
@@ -0,0 +1,18 @@
+// Array
+
+const animals = [
+    'lion',
+    'monkey',
+    'cat'
+]
+
+// acessar valores dentro do Array
+console.log(animals[0])
+console.log(animals[1])
+console.log(animals[2])
+
+// colocando em uma frase
+console.log()
+console.log(`Tenho um ${animals[0]} em casa.`)
+console.log(`E eu tenho dois ${animals[1]} aqui.`)
+console.log(`E eu tenho um lindo ${animals[2]}.`)
