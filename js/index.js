d89509bbea6d2a7efc968be2fe497b963ef5a708
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-13 16:54:20 -0300

    Praticando e avançando no Javascript
    Declaration assignment var;
    
    Agrupando declarações;
    
    Concatenando e interpolando variáveis;

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Variáveis e tipos de dados
+//  Declaração or declaration
+var name
+
+//  assignment or atribuição de valores
+name = "Mike"
+
+// que tipo de dado foi colocado na variável ?
+console.log(typeof name)
+
+// agrupamento de delcarações
+let age, isHuman
+
+age = 18
+isHuman = true
+
+// multiplos argumentos na função
+console.log(name, age, isHuman)
+
+// escrita de texto + variáveis
+console.log('o ' + name + ' tem ' + age + ' anos e ele é ' + isHuman + '.')
+
+// interpolando valores com templetes literals or template strings
+console.log(`my name is ${name}, my age ${18}, i am ${isHuman}`)
+
