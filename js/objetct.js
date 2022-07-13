991696405c29cfd43d884f6b80f7a5f5279542ef
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-13 17:00:37 -0300

    Objetos

---
+++ js/objetct.js
@@ -0,0 +1,20 @@
+const person = {
+    name: 'Elivan',
+    age: 30,
+    weight: 79.8,
+    isAdmin: true
+}
+
+// imprimindo objeto completo
+// console.log(person)
+
+// pegando valor da propriedade dentro do objeto
+console.log(person.name)
+console.log(person.age)
+console.log(person.weight)
+console.log(person.isAdmin)
+
+console.log()
+
+// usando em uma frase
+console.log(`Eu me chamo ${person.name} tenho ${person.age} anos minha altura é ${person.weight} e isso é ${person.isAdmin}.`)
